425fb1cee20e5d743d7ce45edc80a923c2952411
Keane <keanecodes@gmail.com>
2019-10-02 15:02:11 +0800

    Add firebase and folder restructure

---
+++ firebase/functions/index.js
@@ -0,0 +1,11 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld =
+  functions
+  .region('asia-east2')
+  .https.onRequest((request, response) => {
+ response.send("Hello from Bookator!");
+});
